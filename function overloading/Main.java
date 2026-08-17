ba21385046a60cdf3a337d09a526fc60b91a8237
Abhay Tiwari <abhayq54@gmail.com>
2026-08-17 14:11:00 +0530

    Add files via upload

---
+++ function overloading/Main.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the value of x:");
+        int x=sc.nextInt();
+         
+  
+        for(int i=1; i<=10;i++){
+                 System.out.println(x+"X"+i+"="+x*i);
+            }
+    }
+}
